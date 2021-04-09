d1c4a9705f8da5966c97c2c2dbadb02f4fba2c9d
Sherryl Sugiono Sindarto <62754325+sherrylsin20@users.noreply.github.com>
2021-04-09 13:03:26 +0700

    Orders model modification

--- lib/screens/models/orders.dart
+++ lib/screens/models/orders.dart
@@ -7,21 +7,24 @@ class Orders {
   static const UUID = "uuid";
   static const TOTAL = "total";
   static const STATUS = "status";
-  static const CREATED_AT = "createdAt";
+  static const DELIV_DATE = "deliv_date";
+  static const CREATED_AT = "created_at";
 
   String _id_order;
   String _id_resto;
   String _uuid;
   String _status;
   int _total;
-  int _createdAt;
+  int _deliv_date;
+  int _created_at;
 
   String get id_order => _id_order;
   String get id_resto => _id_resto;
   String get uuid => _uuid;
   String get status => _status;
   int get total => _total;
-  int get createdAt => _createdAt;
+  int get deliv_date => _deliv_date;
+  int get created_at => _created_at;
 
   List cart;
 
@@ -31,7 +34,8 @@ class Orders {
     _uuid = snapshot.data()[UUID];
     _status = snapshot.data()[STATUS];
     _total = snapshot.data()[TOTAL];
-    _createdAt = snapshot.data()[CREATED_AT];
+    _deliv_date = snapshot.data()[DELIV_DATE];
+    _created_at = snapshot.data()[CREATED_AT];
     cart = snapshot.data()[CART];
   }
 }

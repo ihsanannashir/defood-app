27af9cac4550eaccb0670033e2d2fb5a484d67f9
greatspy <ihsanannashir@gmail.com>
2021-04-09 11:07:18 +0700

    Finished Account Page and Register

--- lib/screens/pageview/akun.dart
+++ lib/screens/pageview/akun.dart
@@ -3,6 +3,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:styled_text/styled_text.dart';
 
 class Akun extends StatefulWidget {
   @override
@@ -12,7 +14,8 @@ class Akun extends StatefulWidget {
 class _AkunState extends State<Akun> {
   final searchController = TextEditingController();
   FirebaseAuth _auth = FirebaseAuth.instance;
-  static String _email, _username;
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+  static String _email, _username, _fullname;
   String _timeString;
   Timer _timer;
 
@@ -45,10 +48,22 @@ class _AkunState extends State<Akun> {
     });
   }
 
+  void getUserName() {
+    var firebaseUser = _auth.currentUser;
+    _db.collection("users").doc(firebaseUser.uid).get().then((value){
+      //TEST DEBUG
+      print(value.data()["username"]);
+      print(value.data()["fullname"]);
+      _fullname = value.data()["fullname"];
+      _username = value.data()["username"];
+    });
+  }
+
   @override
   void initState() {
     super.initState();
     getCurrentUser();
+    getUserName();
     _timer = Timer.periodic(Duration(seconds: 0), (Timer t) => _getDay());
   }
 
@@ -67,14 +82,74 @@ class _AkunState extends State<Akun> {
                       vertical: 20.0,
                     ),
                     child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Text("Halo, Selamat ${_timeString} ${_email}",
+                        Text("Halo, Selamat ${_timeString} ${_username}",
                             style: TextStyle(
                               fontSize: 17.0,
                               color: Colors.white,
                               fontFamily: 'Inter',
-                            )),
+                            )
+                        ),
                         SizedBox(height: 15.0),
+                        _thumb(),
+                        SizedBox(height: 20.0),
+                        Text(
+                          'Nama Lengkap',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'Inter',
+                            fontSize: 17.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 10.0),
+                        Text(
+                          _fullname,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'Inter',
+                            fontSize: 17.0,
+                          ),
+                        ),
+                        SizedBox(height: 20.0),
+                        Text(
+                          'Nama Pengguna',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'Inter',
+                            fontSize: 17.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 10.0),
+                        Text(
+                          _username,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'Inter',
+                            fontSize: 17.0,
+                          ),
+                        ),
+                        SizedBox(height: 20.0),
+                        Text(
+                          'Alamat Email',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'Inter',
+                            fontSize: 17.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 10.0),
+                        Text(
+                          _email,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'Inter',
+                            fontSize: 17.0,
+                          ),
+                        ),
                       ],
                     )),
               ],
@@ -84,4 +159,30 @@ class _AkunState extends State<Akun> {
       ),
     );
   }
+
+  Widget _thumb() {
+    return Wrap(
+      direction: Axis.horizontal,
+      crossAxisAlignment: WrapCrossAlignment.start,
+      spacing: 5,
+      runSpacing: 5,
+      children: [
+        Icon(
+          Icons.person_pin,
+          color: Colors.white,
+          size: 70.0,
+        ),
+        StyledText(
+          text: '<white><bold><big>${_username}</big></bold><br/><small>Pengguna</small></white>',
+          styles: {
+            'bold': TextStyle(fontWeight: FontWeight.bold),
+            'white': TextStyle(color: Colors.white),
+            'big': TextStyle(fontSize: 30.0,),
+            'small': TextStyle(fontSize: 20.0),
+          },
+        )
+      ],
+    );
+  }
+
 }
